4476ea0fb793ac943e24f13c2edba3a8b26c8e50
Leonardo Salom <leosamu@gmail.com>
2017-07-18 08:45:53 +0200

    added accordion effect

--- src/dashboard.js
+++ src/dashboard.js
@@ -1,4 +1,11 @@
     $(document).ready(function() {
+      $(".accordiontable tbody tr:not(.accordion)").hide();
+      $(".accordiontoggle").click(function()
+      {
+        console.log("this");
+        $(this).toggleClass("zmdi-unfold-less zmdi-unfold-more");
+        $(this).parent().parent().nextUntil("tr.accordion").toggle();
+      });
 
       $('.settings-btn').on('click', function (e) {
             $('.settings-btn').toggleClass('open');
